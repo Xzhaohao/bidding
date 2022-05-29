2bc8fcf72e72be670fdcca84859d67d6812ad84c
Liella <13557511525@139.com>
2022-05-29 10:06:26 +0800

    成交公示卡片

--- bidding-web/src/components/notify-card/index.vue
+++ bidding-web/src/components/notify-card/index.vue
@@ -1,13 +1,40 @@
 <template>
+  <div class="card-container">
+    <title-header :title="title" :link="link"/>
 
+    <ul>
+      <li v-for="(item, index) in list" :key="index">
+        <el-link :href="item.link">{{ index + 1 }}. {{ item.title }}</el-link>
+      </li>
+    </ul>
+  </div>
 </template>
 
 <script setup lang="ts">
-export default {
-  name: 'index'
-}
+import { defineProps } from 'vue'
+import TitleHeader from '@/components/title-header/index.vue'
+
+defineProps({
+  title: {
+    type: String
+  },
+  link: {
+    type: String
+  },
+  list: {
+    type: Array
+  }
+})
 </script>
 
 <style scoped lang="scss">
+@import "index.scss";
+</style>
 
+<style lang="scss">
+.card-container .el-link__inner {
+  display: block;
+  width: 100%;
+  @include line-text-overflow;
+}
 </style>
--- bidding-web/src/views/home/index.vue
+++ bidding-web/src/views/home/index.vue
@@ -2,7 +2,8 @@
   <div class="home-container">
     <section class="visual">
       <div class="notify-rule">
-        <title-header title="成交公示" link="/register" />
+        <notify-card title="成交公示" link="/register" :list="list" />
+        <notify-card title="政策法规" link="/register" :list="list" />
       </div>
     </section>
 
@@ -10,7 +11,16 @@
 </template>
 
 <script setup lang="ts">
-import TitleHeader from '@/components/title-header/index.vue'
+import { ref } from 'vue'
+import NotifyCard from '@/components/notify-card/index.vue'
+
+const list = ref([
+    { title: '中国—东盟信息港股份有限公司智能化网络设备采购项目 竞争性谈判公告竞争性谈判公告竞争性谈判', link: '/404' },
+    { title: '中国—东盟信息港股份有限公司智能化网络设备采购项目 竞争性谈判公告', link: '/404' },
+    { title: '中国—东盟信息港股份有限公司智能化网络设备采购项目 竞争性谈判公告', link: '/404' },
+    { title: '中国—东盟信息港股份有限公司智能化网络设备采购项目 竞争性谈判公告', link: '/404' },
+    { title: '中国—东盟信息港股份有限公司智能化网络设备采购项目 竞争性谈判公告', link: '/404' }
+])
 </script>
 
 <style scoped lang="scss">
